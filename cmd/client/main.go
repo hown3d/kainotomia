7581d048072cb8a9dd4e1b1372ac751a5fa490fb
hown3d <ludi.origin@gmail.com>
2022-02-24 02:01:11 +0100

    fix: oauth2 flow on local client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/hown3d/kainotomia/pkg/spotify/auth"
-	"github.com/pkg/browser"
+	"golang.org/x/oauth2"
 )
 
 var url *string = flag.String("url", "http://localhost:8080", "")
 
 func main() {
 	flag.Parse()
+	ch := make(chan *oauth2.Token)
 	log.Println("Serving on :8080")
 	go func() {
-		err := auth.StartHTTPCallbackServer()
+		err := auth.StartHTTPCallbackServer(ch)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
-	browser.OpenURL(*url)
+	token := <-ch
+	fmt.Print(token)
 }
--- pkg/spotify/auth/auth.go
+++ pkg/spotify/auth/auth.go
@@ -1,13 +1,17 @@
 package auth
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 
-	"github.com/hown3d/kainotomia/pkg/file"
 	"github.com/hown3d/kainotomia/pkg/random"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
@@ -18,25 +22,33 @@ const (
 	clientID    = "2893fb6c7e2c44b8bd85a6a0a8d14033"
 )
 
-var state = random.String(20)
+var (
+	state = random.String(10)
+	// codeVerifier must be minimum 32 bytes long
+	codeVerifier = generateCodeVerifier(32)
+)
 
-func StartHTTPCallbackServer() error {
+func StartHTTPCallbackServer(ch chan<- *oauth2.Token) error {
 	authenticator := newSpotifyAuthenticator()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/callback", authHandler(authenticator))
+	mux.HandleFunc("/callback", authHandler(authenticator, ch))
 	mux.HandleFunc("/", redirectHandler(authenticator))
 	return http.ListenAndServe(":8080", mux)
 }
 
 func redirectHandler(authenticator *spotifyauth.Authenticator) func(w http.ResponseWriter, r *http.Request) {
-	url := authURL(authenticator)
 	return func(w http.ResponseWriter, r *http.Request) {
+		url, err := authURL(authenticator)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		log.Println("recived redirect")
+		w.Header().Add("Cache-Control", "no-cache")
 		http.Redirect(w, r, url, http.StatusPermanentRedirect)
 	}
 }
 
-func authHandler(authenticator *spotifyauth.Authenticator) func(w http.ResponseWriter, r *http.Request) {
+func authHandler(authenticator *spotifyauth.Authenticator, ch chan<- *oauth2.Token) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("recived auth")
 		st := r.FormValue("state")
@@ -44,22 +56,24 @@ func authHandler(authenticator *spotifyauth.Authenticator) func(w http.ResponseW
 			http.NotFound(w, r)
 			log.Fatalf("State mismatch: %s != %s\n", st, state)
 		}
-		token, err := authenticator.Token(r.Context(), st, r)
+		token, err := authenticator.Token(r.Context(), st, r, oauth2.SetAuthURLParam("code_verifier", codeVerifier))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
-		file, err := file.ForToken()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Fatal(err)
-		}
+		// file, err := file.ForToken()
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	log.Fatal(err)
+		// }
 
-		err = persistToken(file, token)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Fatal(err)
-		}
+		// err = persistToken(file, token)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	log.Fatal(err)
+		// }
+		ch <- token
+		w.Header().Add("Cache-Control", "no-cache")
 		w.WriteHeader(http.StatusOK)
 	}
 }
@@ -84,9 +98,40 @@ func persistToken(w io.Writer, token *oauth2.Token) error {
 	return nil
 }
 
-func authURL(authenticator *spotifyauth.Authenticator) string {
+func authURL(authenticator *spotifyauth.Authenticator) (string, error) {
+	codeChallenge, err := generateCodeChallenge(codeVerifier)
+	if err != nil {
+		return "", fmt.Errorf("generating code challenge: %w", err)
+	}
 	return authenticator.AuthURL(state,
 		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
-		oauth2.SetAuthURLParam("code_challenge", random.String(20)),
-	)
+		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
+	), nil
+}
+
+func generateCodeChallenge(verifier string) (string, error) {
+	hash := sha256.New()
+	_, err := hash.Write([]byte(verifier))
+	if err != nil {
+		return "", fmt.Errorf("hashing verifier string: %w", err)
+	}
+	encoded := encode(hash.Sum(nil))
+	return encoded, nil
+}
+
+func encode(msg []byte) string {
+	encoded := base64.StdEncoding.EncodeToString(msg)
+	encoded = strings.Replace(encoded, "+", "-", -1)
+	encoded = strings.Replace(encoded, "/", "_", -1)
+	encoded = strings.Replace(encoded, "=", "", -1)
+	return encoded
+}
+
+func generateCodeVerifier(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := 0; i < length; i++ {
+		b[i] = byte(r.Intn(255))
+	}
+	return encode(b)
 }
